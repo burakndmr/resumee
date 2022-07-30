cb9568eae98a4440524e30fa22a12d62db866143
Burak Demir <buraknasifdemir123@gmail.com>
2022-07-30 19:18:01 +0300

    added responsive in FeaturesSection.tsx

--- components/landing-page/section-features/FeaturesSection.tsx
+++ components/landing-page/section-features/FeaturesSection.tsx
@@ -17,19 +17,19 @@ export const FeaturesSection: React.FC<FeaturesSectionProps> = () => {
       title: "Fast CV Builder",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi fuga vel! Ea alias corporis voluptas corrupti, excepturi dolorem deleniti!",
-      icon: "icon-fast-cv-builder",
+      icon: "./landingPage/lightningLogo.svg",
     },
     {
       title: "Fast CV Builder",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi fuga vel! Ea alias corporis voluptas corrupti, excepturi dolorem deleniti!",
-      icon: "icon-fast-cv-builder",
+      icon: "./landingPage/notAccountLogo.svg",
     },
     {
       title: "Fast CV Builder",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nisi fuga vel! Ea alias corporis voluptas corrupti, excepturi dolorem deleniti!",
-      icon: "icon-fast-cv-builder",
+      icon: "./landingPage/saveAndDownloadLogo.svg",
     },
   ];
   return (
@@ -41,14 +41,14 @@ export const FeaturesSection: React.FC<FeaturesSectionProps> = () => {
         <h1 className="mb-20 font-bold text-3xl mx-2 sm:text-4xl sm:mx-0  text-titleBlack leading-10 ">
           CREATE CV, RESUME AND BLA BLA
         </h1>
-        <div className="flex flex-col sm:flex-row  justify-evenly items-center">
+        <div className="flex flex-col  md:flex-row md:flex-wrap gap-4 justify-evenly items-center">
           {features.map((feature, index) => (
             <div
               key={index}
-              className="flex flex-col justify-between items-center w-73 mb-4 sm:mb-0 shadow-lg bg-white rounded-lg p-8 "
+              className="flex flex-col justify-between items-center w-73 mb-4 lg:mb-0 shadow-lg bg-white rounded-lg p-8 "
             >
-              <div className="flex items-center justify-center bg-yellow text-white rounded-full">
-                {feature.icon}
+              <div className=" flex items-center justify-center bg-yellow text-white rounded-full w-24 h-24  mb-12">
+                <img src={feature.icon} alt={feature.title} />
               </div>
               <h3 className="font-semibold text-xl mb-4">{feature.title}</h3>
               <p className="text-sm font-normal text-textGray">
