cbf223d662b50993acf3d61538dab085d2820b29
Burak Demir <buraknasifdemir123@gmail.com>
2022-12-19 12:38:29 +0300

    fix bug: the application crashes if entering the newResume link directly

--- pages/app/resumebuilder/[id].tsx
+++ pages/app/resumebuilder/[id].tsx
@@ -24,6 +24,13 @@ import {
 } from "../../../slices/resumeActions/resumeActionSlice";
 
 const App: NextPage = () => {
+  const router = useRouter();
+
+  const dispatch = useDispatch();
+  const resumes = useSelector(selectAllResumes);
+
+  const isNewResume: Boolean = router.query.id === "newResume";
+
   // Initial Values for the form
   const initialValue = {
     id: "",
@@ -79,30 +86,12 @@ const App: NextPage = () => {
     },
   };
 
-  // For initial form values
-  const [initialState, setInitialState] = useState(initialValue);
-
-  const router = useRouter();
-
-  const dispatch = useDispatch();
-  const resumes = useSelector(selectAllResumes);
-
-  const isNewResume: Boolean = router.query.id === "newResume";
-
   const selectedResumeArr = resumes.filter(
     (resume: Resume) => resume.id === router.query.id
   );
 
-  useEffect(() => {
-    if (isNewResume) {
-      setInitialState(initialValue);
-    } else {
-      setInitialState(selectedResumeArr[0]);
-    }
-  }, []);
-
   const formik = useFormik({
-    initialValues: initialState,
+    initialValues: isNewResume ? initialValue : selectedResumeArr[0],
     enableReinitialize: true,
     onSubmit: (values) => {
       if (isNewResume) {
@@ -115,28 +104,33 @@ const App: NextPage = () => {
 
   return (
     <>
-      <h1>{initialState?.mainInfo?.name}</h1>
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          name="mainInfo.name"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.mainInfo.name}
-        />
-        <input
-          id="phone"
-          name="mainInfo.phone"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.mainInfo.phone}
-        />
-
-        <button onClick={() => Router.push("/app/dashboard")} type="submit">
-          YAZDIR
-        </button>
-      </form>
+      {formik.values?.mainInfo?.name === undefined ? (
+        <p>LOADING</p>
+      ) : (
+        <>
+          <h1>{formik?.values?.mainInfo?.name}</h1>
+          <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              name="mainInfo.name"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.mainInfo.name}
+            />
+            <input
+              id="phone"
+              name="mainInfo.phone"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.mainInfo.phone}
+            />
+            <button onClick={() => Router.push("/app/dashboard")} type="submit">
+              YAZDIR
+            </button>
+          </form>
+        </>
+      )}
     </>
   );
 };
