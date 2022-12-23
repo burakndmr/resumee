8986bccb2cfc6f40ca77a80b2b752d6cc59a6dc3
Burak Demir <buraknasifdemir123@gmail.com>
2022-12-23 22:25:05 +0300

    create first page of form and little tailwind fix

--- pages/app/resumebuilder/[id].tsx
+++ pages/app/resumebuilder/[id].tsx
@@ -14,6 +14,7 @@ import { Alert, Card } from "flowbite-react";
 
 // ------------------ CUSTOM COMPS ------------------
 import { Progress } from "../../../components/resumeCreator/cvBuilder/progress/Progress";
+import { Button } from "../../../components/landing-page/button/Button";
 
 import { BuilderLayout } from "../../../components/resumeCreator/cvBuilder/layout/CvBuilderLayout";
 
@@ -101,6 +102,8 @@ const App: NextPage = () => {
       name: Yup.string().required("Please enter your name"),
       phone: Yup.string().required("Required"),
       email: Yup.string().email("Invalid email").required("Required"),
+      city: Yup.string(),
+      jobTitle: Yup.string(),
     }),
   });
 
@@ -123,16 +126,21 @@ const App: NextPage = () => {
         <p>LOADING</p>
       ) : (
         <BuilderLayout>
-          <div className="pt-10 md:pt-14 lg:pt-36">
+          <div className="pt-10 md:pt-14 lg:pt-28">
             <Progress progress={progress} />
             <h1 className="text-4xl md:text-5xl lg:text-6xl my-6 md:my-9 lg:my-12 text-center font-bold font-sans">
               Tell us a little about yourself
             </h1>
             <Card>
               <form onSubmit={formik.handleSubmit}>
-                <div className="grid gap-6 mb-6 md:grid-cols-2">
-                  <div>
-                    <label htmlFor="name">Name</label>
+                <div className="grid gap-6 md:grid-cols-2 justify-items-stretch">
+                  <div className="row-start-2 row-end-3 md:row-start-1 md:row-end-2">
+                    <label
+                      className="text-md font-semibold text-gray-900"
+                      htmlFor="name"
+                    >
+                      Full Name
+                    </label>
                     <input
                       className={
                         formik.errors.mainInfo?.name &&
@@ -143,6 +151,7 @@ const App: NextPage = () => {
                       id="name"
                       name="mainInfo.name"
                       type="text"
+                      placeholder="John Doe"
                       onChange={formik.handleChange}
                       value={formik.values.mainInfo.name}
                     />
@@ -155,19 +164,38 @@ const App: NextPage = () => {
                       ) : null}
                     </div>
                   </div>
-                  <div>
-                    <label htmlFor="phone">Name</label>
-                    <input
-                      className="input-normal"
-                      id="phone"
-                      name="mainInfo.phone"
-                      type="text"
-                      onChange={formik.handleChange}
-                      value={formik.values.mainInfo.phone}
-                    />
+                  <div className="md:row-span-2 flex items-center justify-center">
+                    <div className="flex items-center text-center cursor-pointer justify-center bg-primaryClick rounded-full h-32 w-32">
+                      <p className="text-xs text-white">
+                        Add Photo <br /> (coming soon)
+                      </p>
+                    </div>
                   </div>
                   <div>
-                    <label htmlFor="email">Email</label>
+                    <label
+                      className="text-md font-semibold text-gray-900"
+                      htmlFor="jobTitle"
+                    >
+                      Job Title
+                    </label>
+                    <input
+                      className="input-normal"
+                      id="jobTitle"
+                      name="mainInfo.jobTitle"
+                      type="text"
+                      placeholder="Frontend Developer"
+                      onChange={formik.handleChange}
+                      value={formik.values.mainInfo.jobTitle}
+                    />
+                  </div>
+
+                  <div>
+                    <label
+                      className="text-md font-semibold text-gray-900"
+                      htmlFor="email"
+                    >
+                      Email
+                    </label>
                     <input
                       className={
                         formik.errors.mainInfo?.email &&
@@ -178,6 +206,7 @@ const App: NextPage = () => {
                       id="email"
                       name="mainInfo.email"
                       type="text"
+                      placeholder="johndoe@resumee.com"
                       onChange={formik.handleChange}
                       value={formik.values.mainInfo.email}
                     />
@@ -190,21 +219,55 @@ const App: NextPage = () => {
                       ) : null}
                     </div>
                   </div>
+
+                  <div>
+                    <label
+                      className="text-md font-semibold text-gray-900"
+                      htmlFor="phone"
+                    >
+                      Phone
+                    </label>
+                    <input
+                      className={
+                        formik.errors.mainInfo?.email &&
+                        formik.touched.mainInfo?.email
+                          ? "input-error"
+                          : "input-normal"
+                      }
+                      id="phone"
+                      name="mainInfo.phone"
+                      type="text"
+                      placeholder="555 555 55 55"
+                      onChange={formik.handleChange}
+                      value={formik.values.mainInfo.phone}
+                    />
+                    <div>
+                      {formik.errors.mainInfo?.phone &&
+                      formik.touched.mainInfo?.phone ? (
+                        <p className="text-sm text-red-600">
+                          {formik.errors.mainInfo?.phone}
+                        </p>
+                      ) : null}
+                    </div>
+                  </div>
+                  <div className="md:justify-self-end md:col-start-2 md:col-end-3">
+                    <button
+                      type="submit"
+                      className="primary-btn w-full p-5 md:w-auto"
+                      onClick={
+                        isNewResume
+                          ? formik.dirty && formik.isValid
+                            ? () => Router.push("/app/dashboard")
+                            : () => console.log("not ready")
+                          : formik.isValid
+                          ? () => Router.push("/app/dashboard")
+                          : () => console.log("not ready")
+                      }
+                    >
+                      Next
+                    </button>
+                  </div>
                 </div>
-                <button
-                  type="submit"
-                  onClick={
-                    isNewResume
-                      ? formik.dirty && formik.isValid
-                        ? () => Router.push("/app/dashboard")
-                        : () => console.log("not ready")
-                      : formik.isValid
-                      ? () => Router.push("/app/dashboard")
-                      : () => console.log("not ready")
-                  }
-                >
-                  YAZDIR
-                </button>
               </form>
             </Card>
           </div>
