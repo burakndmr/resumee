aa62c89f4344565bd5017c6fad945f02e0f961d8
Burak Demir <buraknasifdemir123@gmail.com>
2022-12-27 20:25:03 +0300

    delete flowbite theming features for theming bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,42 +6,16 @@ import Head from "next/head";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
 
-import { Flowbite } from "flowbite-react";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Flowbite
-        theme={{
-          theme: {
-            // card: {
-            //   children:
-            //     "flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 flex-col p-8",
-            // },
-            tab: {
-              tablist: {
-                tabitem: {
-                  styles: {
-                    underline: {
-                      active: {
-                        on: "text-primary border-b-2 border-primary",
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-        }}
-      >
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Poppins:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <Component {...pageProps} />
-      </Flowbite>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Poppins:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
     </Provider>
   );
 }
