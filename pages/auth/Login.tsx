ebfaf0d111ba2edaba3d706914f2d5ddff24c175
Burak Demir <buraknasifdemir123@gmail.com>
2023-01-20 13:44:57 +0300

    create function redirect after login

--- pages/auth/Login.tsx
+++ pages/auth/Login.tsx
@@ -1,20 +1,40 @@
 import React from "react";
+import Router, { useRouter } from "next/router";
 
 import { MainLayout } from "../../components/landing-page/layout/MainLayout";
 import { Card } from "flowbite-react";
 
 import { FcGoogle } from "react-icons/fc";
 
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+import { auth } from "../../utils/firebase";
+
 interface LoginProps {}
 
 const Login: React.FC<LoginProps> = () => {
+  const route = useRouter();
+
+  const googleProvider = new GoogleAuthProvider();
+  const googleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      route.push("/app/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <MainLayout>
         <Card className="mt-32 max-w-xl mx-auto">
           <div className="flex flex-col">
             <h1 className="font-semibold text-3xl text-gray-900">Login</h1>
-            <button className="flex items-center relative justify-center bg-gray-700 hover:bg-gray-800 rounded py-3 px-5 mt-3">
+            <button
+              onClick={googleLogin}
+              className="flex items-center relative justify-center bg-gray-700 hover:bg-gray-800 rounded py-3 px-5 mt-3"
+            >
               <FcGoogle className=" absolute left-4 text-3xl" />
               <p className="text-white">Login with Google</p>
             </button>
--- utils/firebase.ts
+++ utils/firebase.ts
@@ -22,7 +22,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 export const auth = getAuth();
 export const db = getFirestore(app);
